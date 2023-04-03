747041c20554208e1adc410e47beeb87ac3ef443
rohit <rohitsinghind22@gmail.com>
2023-04-03 17:51:17 +0530

    added load more btn

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
-
-// function getNews(catagory,page){
-//     const api = "pub_30647f724fbb0578f2d139ef3b083afedbde"
-//     fetch("https://newsdata.io/api/1/news?apikey="+api+"&language=en&category="+catagory+"&page="+page).then((response)=>{
-//         return response.json();
-//     }).then((data)=>{}
-// }
-
-const arr=[1,2,3,3]
+let x = 9;
 
 const fetchUsers = async ()=>{
   await  fetch("https://xlri.edu/api/xol/sample").then((response) => response.json())
   .then((data) => {
     console.log(data?.data)
-  data?.data?.map((e)=>{
+  data?.data?.filter((item, idx) => idx < x).map((e)=>{
     document.getElementById('studentsBox').innerHTML += "<div class='studentCard'>"+
     "<img class='studentImg' src='https://xlri.edu/images/xol-2022/"+e?.sid+".jpg' alt=''>"+
     "<div class='studentdetails'>"+
@@ -25,5 +17,10 @@ const fetchUsers = async ()=>{
 }
   );
 }
+
+loadMoreButton.addEventListener('click',()=>{
+  x+=9;
+ fetchUsers();
+})
 
 fetchUsers();
